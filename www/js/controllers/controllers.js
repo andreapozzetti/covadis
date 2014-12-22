9f745fd905e7fa7373c068d3b42effdc17e3c3ac
andrea pozzetti <andrepozz@inwind.it>
2014-12-22 15:47:30 +0100

    test

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,5 +1,10 @@
 angular.module('HomeCtrl', []).controller('HomeCtrl', function($scope) {
 
+    geolocation.getCurrentPosition().then(function(data) {
+       //this will execute when the 
+       //AJAX call completes.
+       $scope.location = data;
+       alert($scope.location);
+    });
   
-
 });
